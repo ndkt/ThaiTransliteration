0c267e26c2ef173e6a8257598fc7e52a2f959fc9
ndkt <p.sabpisal@gmail.com>
2021-06-12 21:51:19 +0700

    Update LetterBeforeVowel.kt

--- src/main/kotlin/LetterBeforeVowel.kt
+++ src/main/kotlin/LetterBeforeVowel.kt
@@ -17,11 +17,26 @@ class LetterBeforeVowel: Vowels {
 
     override fun parse(word: String, alphabet: Alphabets) {
         // Search for Vowels and Replace
-            // 1. Search via Segmentation
-            // 2. Search via Individual Splitting
+        // Method 1. Search via Segmentation
+        // a.
+        // b.
+        // c.
+        // Method 2. Search via Individual Splitting
+        // a.
+        // b.
+        // c.
+        // Method 3. Search Using IndexOf
+        // a. If the word contains the 'a' and 'aa' then replace that index with the corresponding vowel.
+
+
         // Search for Alphabet and Replace
-
-
-
     }
+
+
+    fun matchDetails(inputString: String, whatToFind: String, startIndex: Int = 0): String {
+        println("whatToFind: ${whatToFind}, startIndex: ${startIndex}")
+        val matchIndex = inputString.indexOf(whatToFind, startIndex)
+        return "Searching for '$whatToFind' in '$inputString' starting at position $startIndex: " + if (matchIndex >= 0) "Found at $matchIndex" else "Not found"
+    }
+
 }
